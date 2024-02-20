ebcb539b9727740afde72db1590ff99e9c5cc381
KKY <rnjsrldud2003@gmail.com>
2024-02-20 14:19:32 +0900

    Update countdown.js - Implement reset countDown function.

--- banpickScreen/js/countdown.js
+++ banpickScreen/js/countdown.js
@@ -1,26 +1,33 @@
 // implement countdown function (Rough version)
 
-let isCounting = false;
+let intervalId;
+const countText = document.querySelector('.banpick__header-middle__num-count span');
+const leftBar = document.querySelector('.banpick__header-middle__left-bar');
+const rightBar = document.querySelector('.banpick__header-middle__right-bar');
 
 function countDown(sec) {
-	const countText = document.querySelector('.banpick__header-middle__num-count span');
-	const leftBar = document.querySelector('.banpick__header-middle__left-bar');
-	const rightBar = document.querySelector('.banpick__header-middle__right-bar');
-
+	stopCountDown();
 	leftBar.classList.add('bar-decrease-30');
 	rightBar.classList.add('bar-decrease-30');
 
 	let count = sec;
-	if (!isCounting) {
-		isCounting = true;
-		const id = setInterval(() => {
-			if (count === -1) {
-				clearInterval(id);
-				isCounting = false;
-			} else {
-				countText.innerText = count;
-				count--;
-			}
-		}, 1000);
-	}
+
+	intervalId = setInterval(() => {
+		if (count === -1) {
+			clearInterval(intervalId);
+		} else {
+			countText.innerText = count;
+			count--;
+		}
+	}, 1000);
+}
+
+function stopCountDown() {
+	leftBar.classList.remove('bar-decrease-30');
+	rightBar.classList.remove('bar-decrease-30');
+
+	void leftBar.offsetWidth; // trick for reset animation
+	void rightBar.offsetWidth;
+
+	clearInterval(intervalId);
 }
