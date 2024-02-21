030345dcd9235ebf56c0c2a40a9e006c88688eb1
KKY <rnjsrldud2003@gmail.com>
2024-02-21 16:11:01 +0900

    Update variables.js - add variables of requestChampName.js file.

--- banpickScreen/js/requestChampName.js
+++ banpickScreen/js/requestChampName.js
@@ -1,9 +1,5 @@
 // create default banpick screen
 
-const championsInfoURL = 'https://ddragon.leagueoflegends.com/cdn/14.3.1/data/en_US/champion.json';
-const championsDiv = document.querySelector('.banpick__body-middle__champions');
-const allChampNames = [];
-
 function getAllChampNames(array, jsonData) {
 	for (let name in jsonData) {
 		array.push(name);
